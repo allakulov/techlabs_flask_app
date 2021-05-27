3f31d6238a981cd5f5cfbd604af4917a4086c437
RAMAMURTHI <ramamurthi@Administrators-MacBook-Pro-135.local>
2021-05-27 17:33:43 +0530

    Adding process.py

---
+++ process.py
@@ -0,0 +1,11 @@
+from fastbook import *
+from fastai.vision.widgets import *
+
+bears = DataBlock(
+    blocks=(ImageBlock, CategoryBlock), 
+    get_items=get_image_files, 
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=Resize(128))
+
+    
