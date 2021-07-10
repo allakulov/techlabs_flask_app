eb236ff003f185cfbbebf15dfaa107d0ad7b0f8b
RAMAMURTHI <ramamurthi@Administrators-MacBook-Pro-135.local>
2021-07-10 22:40:46 +0530

    Working sandbox

--- sandbox.py
+++ sandbox.py
@@ -1,3 +1,6 @@
+import fastbook
+fastbook.setup_book()
+
 from fastbook import *
 from fastai.vision.widgets import *
 
@@ -8,9 +11,16 @@ model_path = (path/MODEL_NAME)
     
 learn_inf = load_learner(model_path)
 
-img_path = (path/'uploads'/'test.jpg')
-pred , pred_idx , probs = learn_inf.predict(img_path)
+import torchvision.transforms as tfms
+
+def pil2fast(img):  
+  return Image(tfms.ToTensor()(img))
+
+img = PILImage.create('uploads/test.jpg')
+image2tensor(img)
+pred , pred_idx , probs = learn_inf.predict(img)
 
 out = f'Prediction:{pred} : probability: {probs[pred_idx]:.04f}'
 
 print(out)
+
