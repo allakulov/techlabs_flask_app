dcd1484ebcbdd7d7f491a6cae468cdcdcf7e00cf
Umrbek Allakulov <u.allakulov@gmail.com>
2022-07-18 15:02:00 +0200

    removed predictapi

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def model_predict(img_path, model_path):
     
     prob_value = probs[pred_idx] * 100 
     
-    out = f'Our machine learning model has predicted the image to be {pred} with {prob_value:.02f} % confidence !'
+    out = f'Our AI thinks the item in your image is {pred} with {prob_value:.02f} % confidence.'
 
     return out
 
@@ -56,56 +56,28 @@ def upload():
         return out
     return None
 
-@app.route('/predictapi', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+# @app.route('/predictapi', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         # Get the file from post request
+#         f = request.files['file']
 
-        # Save the file to ./uploads
-        basepath = Path(__file__).parent
-        file_path = basepath.joinpath('uploads')
-        create_directory_if_not_exists(file_path)
-        filename = 'test.jpg'
-        file_path = file_path.joinpath(filename)
-        f.save(file_path)
+#         # Save the file to ./uploads
+#         basepath = Path(__file__).parent
+#         file_path = basepath.joinpath('uploads')
+#         create_directory_if_not_exists(file_path)
+#         filename = 'test.jpg'
+#         file_path = file_path.joinpath(filename)
+#         f.save(file_path)
 
-        # Make prediction
-        learn_inf = load_learner(model_path)
-        pred , pred_idx , probs = learn_inf.predict(file_path)
-        prob_value = probs[pred_idx] * 100 
-        result = {'prediction': pred, 'probability': prob_value}
+#         # Make prediction
+#         learn_inf = load_learner(model_path)
+#         pred , pred_idx , probs = learn_inf.predict(file_path)
+#         prob_value = probs[pred_idx] * 100 
+#         result = {'prediction': pred, 'probability': prob_value}
 
-        return jsonify(result)
-    return None
-
-# @app.route('/predictapi',methods=['POST'])
-# def predictapi():
-#     '''
-#     For rendering results on HTML GUI
-#     '''
-#     img = request.files.get('file')
-
-#     if not img or not img.filename:
-#         raise BadRequest("You need to upload a file")
-
-#     basepath = Path(__file__).parent
-#     file_path = basepath.joinpath('uploads')
-#     create_directory_if_not_exists(file_path)
-#     filename = 'test.jpg'
-#     file_path = file_path.joinpath(filename)
-#     img.save(file_path)    
-
-#     path = Path()
-#     model_path = (path/MODEL_NAME)
-#     learn_inf = load_learner(model_path)
-#     pred , pred_idx , probs = learn_inf.predict(file_path)
-#     prob_value = probs[pred_idx] * 100 
-#     result = {'prediction': pred, 
-#     'probability': prob_value
-#     }
-
-#     return jsonify(result)
+#         return jsonify(result)
+#     return None
 
 
 if __name__ == '__main__':
